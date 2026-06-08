c08fbdad15105caa04a2718f67b332214b96ecf2
rushant.kangude <rushant.kangude730@gmail.com>
2026-06-08 14:20:46 +0530

    Project Added

---
+++ AuditService/src/test/java/com/exponent/lms/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exponent.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
